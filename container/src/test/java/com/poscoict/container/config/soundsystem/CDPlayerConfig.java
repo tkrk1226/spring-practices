606cefd61cadcc6f1a1d4d8aa3527bd10d877359
UijinJung <tkrk1226@ajou.ac.kr>
2022-02-07 15:40:39 +0900

    wiring [220207-1540]

---
+++ container/src/test/java/com/poscoict/container/config/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,16 @@
+package com.poscoict.container.config.soundsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="com.poscoict.container.soundsystem")
+public class CDPlayerConfig {
+	
+//	@Bean
+//	public DataSource dataSource() {
+//		BasicDataSource ds = new BasicDataSource();
+//		ds.set...
+//	}
+}
--- container/src/test/java/com/poscoict/container/soundsystem/CDPlayerJavaConfigTest.java
+++ container/src/test/java/com/poscoict/container/soundsystem/CDPlayerJavaConfigTest.java
@@ -11,9 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.poscoict.container.config.soundsystem.CDPlayerConfig;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations= {"classpath:com/poscoict/container/config/soundsystem/CDPlayerConfig.xml"})
-public class CDPlayerXmlConfigTest {
+@ContextConfiguration(classes=CDPlayerConfig.class)
+public class CDPlayerJavaConfigTest {
 
 	@Rule
 	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
@@ -22,19 +24,14 @@ public class CDPlayerXmlConfigTest {
 	private CDPlayer cdPlayer;
 	
 	@Test
-	public void testCDPlayerNotNull() {		
+	public void testCDPlayerNot() {
 		assertNotNull(cdPlayer);
 	}
-
+	
 	@Test
 	public void testPlay() {
 		cdPlayer.play();
-	
 		assertEquals("Playing 붕붕 by 김하온", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
-//		linux는 \r\n , window는 \n 이므로 둘다 제거
-//		문제에서 요구하는대로 풀어야하는 이유... 
-//      -> 이 기능들을 응용하면 코테에서 검증가능	
-//		assertTrue(1 == 2 - 1);
 	}
 	
 }
